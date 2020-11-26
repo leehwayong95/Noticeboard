b084b6b7f134164b31b61b182b67a3c9b657f77e
SeongHun_Cho <jng00153@gmail.com>
2020-11-26 17:58:49 +0900

    html test case

---
+++ Vue/NoticeBoard-client/src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="test">
+        <h1>테스트용</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- Vue/NoticeBoard-client/src/router/index.js
+++ Vue/NoticeBoard-client/src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+// import Home from '@/components/Home'
+// import Login from '@/components/Login'
+// import Register from '@/components/Register'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'testpage',
+      component:Test
     }
   ]
 })
