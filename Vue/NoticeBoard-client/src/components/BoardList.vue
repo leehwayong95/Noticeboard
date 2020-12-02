d8670e421bbf508d2940eee3fe1b8f88b64f7606
SeongHun_Cho <jng00153@gmail.com>
2020-12-02 15:14:39 +0900

    spring과 vue 분리시키기

---
+++ Vue/NoticeBoard-client/src/components/BoardList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-table
+            striped
+        hover
+        :items="items"
+        :per-page="perPage"
+        :current-page="currentPage"
+        :fields="fields"
+        @row-clicked="rowClick"
+        ></b-table>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
--- Vue/NoticeBoard-client/src/components/SignUp.vue
+++ Vue/NoticeBoard-client/src/components/SignUp.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="sign-up">
         <p>회원가입</p>
-        <input type="text" placeholder="text"><br><br>
+        <input type="text" placeholder="userid"><br><br>
         <input type="password" placeholder="password"><br><br>
         <button>가입하기</button>
     </div>
 </template>
 
 <script>
+export default {
+  name: 'signUp',
+  data () {
+    return {
+
+    }
+  },
+  methods: {}
+}
 </script>
 
 <style scoped>
