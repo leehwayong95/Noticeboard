ce22016e19c688e4151119363885768c0913529a
Lee Hwayong <dlghkdyd500@gmail.com>
2020-12-07 15:14:04 +0900

    Add Router guard on Vue.js at write

--- Vue/NoticeBoard-client/src/router/index.js
+++ Vue/NoticeBoard-client/src/router/index.js
@@ -40,36 +40,43 @@ const router = new Router({
       name: 'board',
       component: board,
       beforeEnter: (to, from, next) => {
-        let findflag = null
-        let cookieArr = document.cookie.split(';')
-        for (let i in cookieArr) {
-          if (cookieArr[i].split('=')[0].trim() === 'jwt-auth-token') {
-            findflag = true
-            axios.defaults.headers.common['jwt-auth-token'] = cookieArr[i].split('=')[1].trim()
-            axios.post('http://localhost:9000/api/info')
-              .then((res) => {
-                next()
-              })
-              .catch((error) => {
-                alert('로그인을 다시 해주세요.')
-                console.log(error.message)
-                next('/test')
-              })
-            break
-          }
-        }
-        if (findflag === null) {
-          alert('로그인을 해주세요')
-          next('/test')
-        }
+        checklogin(to, from, next)
       }
     },
     {
       path: '/write',
       name: 'write',
-      component: write
+      component: write,
+      beforeEnter: (to, from, next) => {
+        checklogin(to, from, next)
+      }
     }
   ]
 })
+
+function checklogin (to, from, next) {
+  let findflag = null
+  let cookieArr = document.cookie.split(';')
+  for (let i in cookieArr) {
+    if (cookieArr[i].split('=')[0].trim() === 'jwt-auth-token') {
+      findflag = true
+      axios.defaults.headers.common['jwt-auth-token'] = cookieArr[i].split('=')[1].trim()
+      axios.post('http://localhost:9000/api/info')
+        .then((res) => {
+          next()
+        })
+        .catch((error) => {
+          alert('로그인을 다시 해주세요.')
+          console.log(error.message)
+          next('/test')
+        })
+      break
+    }
+  }
+  if (findflag === null) {
+    alert('로그인을 해주세요')
+    next('/test')
+  }
+}
 
 export default router
