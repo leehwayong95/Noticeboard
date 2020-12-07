ec9e628d6ce4905d8464b34ec553a84df5bb6e12
Lee Hwayong <dlghkdyd500@gmail.com>
2020-12-07 16:25:56 +0900

    edit token expire time section

--- Vue/NoticeBoard-client/src/components/Board.vue
+++ Vue/NoticeBoard-client/src/components/Board.vue
@@ -1,18 +1,46 @@
 <template>
-    <div>
-        <h2>게시판 리스트</h2>
-        <a href="javascript:;" @click="getList">허허</a>
-        <button type="submit" v-on:click ="logout">로그아웃</button>
-    </div>
+  <div>
+      <h2>게시판 리스트</h2>
+      <div class = "list">
+        <table class = "tbList">
+          <tr>
+            <th>no</th>
+            <th>title</th>
+            <th>id</th>
+            <th>date</th>
+          </tr>
+          <tr v-for="(row, postindex) in list" :key="postindex">
+            <td>{{row.postindex}}</td>
+            <td class = "txt_left"><a href="javascript:;">{{row.title}}</a></td>
+            <td>{{row.id}}</td>
+            <td>{{row.date}}</td>
+          </tr>
+        </table>
+      </div>
+      <div class="btnRightWrap">
+        <button type="submit" v-on:click="logout">로그아웃</button>
+        <button type="button" v-on:click="write">글쓰기</button>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
+  mounted () {
+    this.getList()
+  },
+  data () {
+    return {
+      body: '',
+      list: '',
+      no: ''
+    }
+  },
   methods: {
     getList () {
       this.$axios.get('http://localhost:9000/api/board')
         .then((res) => {
-          console.log(res)
+          this.list = res.data
         })
         .then((err) => {
           console.log(err)
@@ -21,6 +49,9 @@ export default {
     logout () {
       document.cookie = 'jwt-auth-token =; expires = Web; 01 Jan 1970'
       this.$router.push('/test')
+    },
+    write () {
+      this.$router.push('/write')
     }
   }
 }
