09182f24211f960b8f770c2469a2de07aaed8283
leehwayong95 <dlghkdyd500@gmail.com>
2021-01-13 12:15:40 +0900

    revert my files

--- leehwayong/Vue/NoticeBoard-client/src/main.js
+++ leehwayong/Vue/NoticeBoard-client/src/main.js
@@ -3,16 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD:leehwayong/Vue/NoticeBoard-client/src/main.js
 import axios from 'axios'
-=======
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueCookie from 'vue-cookie'
-Vue.use(BootstrapVue)
-Vue.use(VueCookie)
->>>>>>> origin/kangyohan:kangyohan/Vue/my-project/src/main.js
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
