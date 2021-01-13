a756186123fd62725763991a88127fc4e6afb05c
SeongHun_Cho <jng00153@gmail.com>
2021-01-13 12:30:13 +0900

    revert file

--- choseonghun/Spring/src/main/java/com/emgram/noticeboard/Model/UserModel.java
+++ choseonghun/Spring/src/main/java/com/emgram/noticeboard/Model/UserModel.java
@@ -1,23 +1,10 @@
 package com.emgram.noticeboard.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserModel {
-	
 	private String id;
 	private String name;
 	private String PW;
-<<<<<<< HEAD:00. default/Spring/src/main/java/com/emgram/noticeboard/Model/UserModel.java
-	private int permission;
-
-=======
 	private String permission;
->>>>>>> origin/Seonghun:choseonghun/Spring/src/main/java/com/emgram/noticeboard/Model/UserModel.java
 	public String getId() {
 		return id;
 	}
@@ -36,18 +23,11 @@ public class UserModel {
 	public void setPW(String PW) {
 		this.PW = PW;
 	}
-<<<<<<< HEAD:00. default/Spring/src/main/java/com/emgram/noticeboard/Model/UserModel.java
-	public int getPermission() {
-=======
 	public String getPermission() {
->>>>>>> origin/Seonghun:choseonghun/Spring/src/main/java/com/emgram/noticeboard/Model/UserModel.java
 		return permission;
 	}
 	public void setPermission(String permission) {
 		this.permission = permission;
 	}
-<<<<<<< HEAD:00. default/Spring/src/main/java/com/emgram/noticeboard/Model/UserModel.java
-=======
 	
->>>>>>> origin/Seonghun:choseonghun/Spring/src/main/java/com/emgram/noticeboard/Model/UserModel.java
 }
--- choseonghun/Vue/NoticeBoard-client/src/main.js
+++ choseonghun/Vue/NoticeBoard-client/src/main.js
@@ -3,12 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD:00. default/Vue/NoticeBoard-client/src/main.js
-import axios from 'axios'
-
-Vue.config.productionTip = false
-Vue.prototype.$axios = axios
-=======
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -20,7 +14,6 @@ Vue.use(VueCookies)
 Vue.config.productionTip = false
 // 쿠키의 만료일은 7일이다. (글로벌 세팅)
 Vue.$cookies.config('7d')
->>>>>>> origin/Seonghun:choseonghun/Vue/NoticeBoard-client/src/main.js
 
 /* eslint-disable no-new */
 new Vue({
